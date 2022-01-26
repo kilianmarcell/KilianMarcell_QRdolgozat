cefc9f7a2be5a923314eebf58733c06520698bab
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-26 19:48:27 +0100

    Inicializálás kész

--- app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
+++ app/src/main/java/hu/petrik/qrdolgozat/MainActivity.java
@@ -1,14 +1,35 @@
 package hu.petrik.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanButton, kiirButton;
+    private TextView szovegTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        scanButton = findViewById(R.id.scanButton);
+        kiirButton = findViewById(R.id.kiirButton);
+        szovegTextView = findViewById(R.id.szovegTextView);
     }
 }
